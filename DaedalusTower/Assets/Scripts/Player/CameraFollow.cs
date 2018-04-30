174b0baa88f62980cf1fa401ec6b32a2b2b1dca4
AnonymousMadman2 <joshuamvac@gmail.com>
2018-04-30 18:32:32 +1000

    Camera adjustments
    Almost there, just need to fix distance bug that has regrown

--- DaedalusTower/Assets/Scripts/Player/CameraFollow.cs
+++ DaedalusTower/Assets/Scripts/Player/CameraFollow.cs
@@ -6,7 +6,6 @@ public class CameraFollow : MonoBehaviour {
 
     public GameObject player;
     public float cameraDistance;
-    public float defaultCameraDistance;
 
     public float minCameraDistance;
     public float cameraPadding;
@@ -16,60 +15,99 @@ public class CameraFollow : MonoBehaviour {
     public Vector3 rotateOffset;
 
     public float rotateSpeed;
+    private bool cameraCheck;
 
 	// Use this for initialization
 	void Start () {
+        initOffset.z = -cameraDistance;
         transform.position = player.transform.position + initOffset;
         rotateOffset = initOffset;
         springOffset = initOffset;
+        cameraCheck = false;
         }
 	
 	// Update is called once per frame
 	void Update () {
         springArm();
+        if(Mathf.Sqrt(springOffset.x * springOffset.x + springOffset.z * springOffset.z) > cameraDistance) {
+            cameraCheck = true;
+            springArm();
+        }
         transform.position = player.transform.position + springOffset;
         transform.LookAt(player.transform);
-        
+        //print(Mathf.Sqrt(springOffset.x * springOffset.x + springOffset.z * springOffset.z));
+        //print(Mathf.Sqrt(initOffset.x * initOffset.x + initOffset.z * initOffset.z));
     }
 
     public void cameraRotate(float mouseValueX) {
         transform.RotateAround(player.transform.position, player.transform.up, rotateSpeed * Time.deltaTime * mouseValueX);
         rotateOffset.x = transform.position.x - player.transform.position.x;
         rotateOffset.z = transform.position.z - player.transform.position.z;
-        if (rotateOffset.x > cameraDistance) {
+
+        if (rotateOffset.x > cameraDistance + cameraPadding) {
             rotateOffset.x = cameraDistance;
-        } else if (rotateOffset.x < -cameraDistance) {
+        } else if (rotateOffset.x < -cameraDistance - cameraPadding) {
             rotateOffset.x = -cameraDistance;
         }
 
-        if (rotateOffset.z > cameraDistance) {
+        if (rotateOffset.z > cameraDistance + cameraPadding) {
             rotateOffset.z = cameraDistance;
-        } else if (rotateOffset.z < -cameraDistance) {
+        } else if (rotateOffset.z < -cameraDistance - cameraPadding) {
             rotateOffset.z = -cameraDistance;
         }
 
         springArm();
 
-        if (springOffset.x > cameraDistance) {
+        if (springOffset.x > cameraDistance + cameraPadding) {
             springOffset.x = cameraDistance;
-        } else if (springOffset.x < -cameraDistance) {
+        } else if (springOffset.x < -cameraDistance - cameraPadding) {
             springOffset.x = -cameraDistance;
         }
 
-        if (springOffset.z > cameraDistance) {
+        if (springOffset.z > cameraDistance + cameraPadding) {
             springOffset.z = cameraDistance;
-        } else if (springOffset.z < -cameraDistance) {
+        } else if (springOffset.z < -cameraDistance - cameraPadding) {
             springOffset.z = -cameraDistance;
         }
     }
 
+    public void playerCounterRotate() {
+        rotateOffset.x = transform.position.x - player.transform.position.x;
+        rotateOffset.z = transform.position.z - player.transform.position.z;
+        if (rotateOffset.x > cameraDistance + cameraPadding) {
+            rotateOffset.x = cameraDistance;
+        } else if (rotateOffset.x < -cameraDistance - cameraPadding) {
+            rotateOffset.x = -cameraDistance;
+        }
 
-    private void springArm() {
+        if (rotateOffset.z > cameraDistance + cameraPadding) {
+            rotateOffset.z = cameraDistance;
+        } else if (rotateOffset.z < -cameraDistance - cameraPadding) {
+            rotateOffset.z = -cameraDistance;
+        }
+
+        springArm();
+
+        if (springOffset.x > cameraDistance + cameraPadding) {
+            springOffset.x = cameraDistance;
+        } else if (springOffset.x < -cameraDistance - cameraPadding) {
+            springOffset.x = -cameraDistance;
+        }
+
+        if (springOffset.z > cameraDistance + cameraPadding) {
+            springOffset.z = cameraDistance;
+        } else if (springOffset.z < -cameraDistance - cameraPadding) {
+            springOffset.z = -cameraDistance;
+        }
+    }
+
+    public void springArm() {
         RaycastHit hit;
         Debug.DrawRay(player.transform.position, transform.position - player.transform.position, Color.blue);
-        if (Physics.Raycast(player.transform.position, transform.position - player.transform.position, out hit, defaultCameraDistance)) {
+        if (Physics.Raycast(player.transform.position, transform.position - player.transform.position, out hit, cameraDistance)) {
             Debug.DrawRay(player.transform.position, transform.position - player.transform.position, Color.red);
             if (hit.collider.tag == "terrain") {
+                cameraCheck = true;
                 springOffset.x = hit.point.x - player.transform.position.x;
                 springOffset.z = hit.point.z - player.transform.position.z;
                 
@@ -78,9 +116,16 @@ public class CameraFollow : MonoBehaviour {
                 springOffset.z = rotateOffset.z;
             }
         } else {
-            cameraDistanceReset();
-            springOffset.x = initOffset.x;
-            springOffset.z = initOffset.z;
+            if (cameraCheck) {
+                cameraDistanceReset();
+                springOffset.x = initOffset.x;
+                springOffset.z = initOffset.z;
+                print(springOffset.z);
+                cameraCheck = false;
+            }else {
+                springOffset.x = rotateOffset.x;
+                springOffset.z = rotateOffset.z;
+            }
         }
     }
 
@@ -102,35 +147,24 @@ public class CameraFollow : MonoBehaviour {
 
         //Find Midpoint on circle at cameraDistance
         newX = cameraDistance * (xPoint.x + zPoint.x) / (Mathf.Sqrt((xPoint.x + zPoint.x) * (xPoint.x + zPoint.x) + (xPoint.z + zPoint.z) * (xPoint.z + zPoint.z)));
-        newZ = (xPoint.z + zPoint.z) / (xPoint.x + zPoint.x) * newX;
+        if (newX == 0) {
+            newZ = zPoint.z;
+        } else {
+            newZ = (xPoint.z + zPoint.z) / (xPoint.x + zPoint.x) * newX;
+        }
         
         initOffset = new Vector3(newX, initOffset.y, newZ);
 
-        if (initOffset.x > cameraDistance) {
+        if (initOffset.x > cameraDistance + cameraPadding) {
             initOffset.x = cameraDistance;
-        } else if (initOffset.x < -cameraDistance) {
-           initOffset.x = -cameraDistance;
+        } else if (initOffset.x < -cameraDistance - cameraPadding) {
+            initOffset.x = -cameraDistance;
         }
 
-        if (springOffset.z > cameraDistance) {
+        if (springOffset.z > cameraDistance + cameraPadding) {
             initOffset.z = cameraDistance;
-        } else if (springOffset.z < -cameraDistance) {
+        } else if (springOffset.z < -cameraDistance - cameraPadding) {
             initOffset.z = -cameraDistance;
         }
     }
-
-
-
-    /*public void cameraSet() {
-        if (Vector3.Distance(new Vector3(transform.position.x, 0, transform.position.z), new Vector3(player.transform.position.x, 0, player.transform.position.z)) > cameraDistance) {
-            transform.position = transform.position + new Vector3(transform.forward.x, 0, transform.forward.z).normalized * player.GetComponent<PlayerMovement>().forwardMoveSpeed * 2 * Time.deltaTime;
-        } else if (Vector3.Distance(new Vector3(transform.position.x, 0, transform.position.z), new Vector3(player.transform.position.x, 0, player.transform.position.z)) < cameraDistance - cameraPadding) {
-            transform.position = transform.position - new Vector3(transform.forward.x, 0, transform.forward.z).normalized * player.GetComponent<PlayerMovement>().forwardMoveSpeed * 2 * Time.deltaTime;
-        } else if (cameraDistance < minCameraDistance) {
-            transform.position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z);
-        }
-        transform.position = player.transform.position + springOffset;
-        transform.LookAt(player.transform);
-    }
-    */
 }
--- DaedalusTower/Assets/Scripts/Player/PlayerMovement.cs
+++ DaedalusTower/Assets/Scripts/Player/PlayerMovement.cs
@@ -22,25 +22,14 @@ public class PlayerMovement : MonoBehaviour {
 
     public void forwardAxisMovement(float direction) {
         transform.position = transform.position + new Vector3(playerCam.transform.forward.x, 0, playerCam.transform.forward.z).normalized * forwardMoveSpeed * -direction * Time.deltaTime;
+        playerCam.transform.position = playerCam.transform.position + new Vector3(playerCam.transform.forward.x, 0, playerCam.transform.forward.z).normalized * forwardMoveSpeed * -direction * Time.deltaTime;
         transform.LookAt(transform.position + new Vector3(playerCam.transform.forward.x, 0, playerCam.transform.forward.z) * -direction);
     }
 
     public void sidewaysAxisMovement(float direction) {
         transform.RotateAround(playerCam.transform.position, transform.up, rotateSpeed * direction * Time.deltaTime);
 
-        playerCam.GetComponent<CameraFollow>().rotateOffset.x = playerCam.transform.position.x - transform.position.x;
-        playerCam.GetComponent<CameraFollow>().rotateOffset.z = playerCam.transform.position.z - transform.position.z;
-        if (playerCam.GetComponent<CameraFollow>().rotateOffset.x > 6) {
-            playerCam.GetComponent<CameraFollow>().rotateOffset.x = 6;
-        } else if (playerCam.GetComponent<CameraFollow>().rotateOffset.x < -6) {
-            playerCam.GetComponent<CameraFollow>().rotateOffset.x = -6;
-        }
-
-        if (playerCam.GetComponent<CameraFollow>().rotateOffset.z > 6) {
-            playerCam.GetComponent<CameraFollow>().rotateOffset.z = playerCam.GetComponent<CameraFollow>().rotateOffset.z = 6;
-        } else if (playerCam.GetComponent<CameraFollow>().rotateOffset.z < -6) {
-            playerCam.GetComponent<CameraFollow>().rotateOffset.z = playerCam.GetComponent<CameraFollow>().rotateOffset.z = -6;
-        }
+        playerCam.GetComponent<CameraFollow>().playerCounterRotate();
 
         transform.LookAt(transform.position + playerCam.transform.right * direction);
     }
