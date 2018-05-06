ff14f2a6dc0f885f4602f3f6ebd23380d3d0d0e1
AnonymousMadman2 <joshuamvac@gmail.com>
2018-05-06 22:35:50 +1000

    NavMeshBeginning
    Just getting some basic NavMesh stuff sorted

---
+++ DaedalusTower/Assets/Scripts/BasicEnemy/EnemyNavigation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyNavigation : MonoBehaviour {
+    
+    public GameObject player;
+
+
+    public Transform destinationPoint;
+    public float minDistance;
+
+	// Use this for initialization
+	void Start () {
+        //destinationPoint = player.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Vector3.Distance(player.transform.position, transform.position) <= minDistance) {
+            destinationPoint.position = transform.position;
+        }else {
+            destinationPoint.position = player.transform.position;
+        }
+        transform.GetComponent<NavMeshAgent>().destination = destinationPoint.position;
+    }
+}
