9bd35d7c25f75d3e51ee43265b8f1f5d61751b75
Gaxsun <Razgriz248@gmail.com>
2018-05-07 02:03:59 +1000

    Initial slope Checking

--- DaedalusTower/Assets/Scripts/Player/PlayerMovement.cs
+++ DaedalusTower/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,19 +5,28 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
     public Camera playerCam;
 
+    public CapsuleCollider capsule;
+
     public float forwardMoveSpeed;
     //public float backwardMoveSpeed;
     public float sidewaysMoveSpeed;
 
     public float rotateSpeed;
 
-	// Use this for initialization
-	void Start () {
+    // 0-1 scaled to player height based off capsule
+    public float slopeCheckHeight;
+
+    // how far to extrude from player
+    public float slopeCheckRadius;
+
+    // Use this for initialization
+    void Start () {
 	}
 	
 	// Update is called once per frame
 	void Update () {
         setRotation();
+        slopeTooSteep();
     }
 
     public void forwardAxisMovement(float direction) {
@@ -30,15 +39,23 @@ public class PlayerMovement : MonoBehaviour {
         playerCam.GetComponent<CameraFollow>().playerCounterRotate();
     }
 
+    // true if too steep
+    public bool slopeTooSteep() {
+        RaycastHit hit;
+        if (Physics.CapsuleCast(new Vector3(transform.position.x, transform.position.y - (capsule.height/2) + (capsule.height * slopeCheckHeight), transform.position.z), new Vector3(transform.position.x, transform.position.y - (capsule.height / 2) + (capsule.height * slopeCheckHeight), transform.position.z), capsule.radius + slopeCheckRadius, new Vector3(0,1,0), out hit)) {
+            if (hit.collider.tag == "terrain") {
+                print("Too Steep");
+            }
+        }        
+        return false;
+    }
+
     private void setRotation() {
         Vector2 joystick = new Vector2(Input.GetAxis("LeftStickX"), Input.GetAxis("LeftStickY"));
         if (joystick.x != 0 || joystick.y != 0) {
             float angle = Mathf.Atan2(joystick.x, -joystick.y) * Mathf.Rad2Deg + playerCam.transform.rotation.eulerAngles.y;
             transform.rotation = Quaternion.Euler(0,angle,0);
         }
-
-
-
 
         //(transform.forward - transform.position) * new Vector3(Input.GetAxis("Horizontal"), 0, -Input.GetAxis("Vertical"));
     }
