e8d3da0d184901a8edc77b120d94ef105deca37d
AnonymousMadman2 <joshuamvac@gmail.com>
2018-05-22 18:42:51 +1000

    Mistwalker movement
    Mistwalker now moves

--- DaedalusTower/Assets/Scripts/mistwalker/mistwalker.cs
+++ DaedalusTower/Assets/Scripts/mistwalker/mistwalker.cs
@@ -13,25 +13,54 @@ public class mistwalker : MonoBehaviour {
     private GameObject player;
     public float minDistance;
 
+    private float fogDensity;
+    public float maxFogDensity;
+    public float fogGrowthRate;
+
+    private float normSpeed;
+    private float normAccel;
     // Use this for initialization
     void Start () {
         player = GameObject.FindGameObjectWithTag("Player");
         player.GetComponent<PlayerMovement>().playerCam.GetComponent<CameraFollow>().bossFight = true;
         RenderSettings.fog = true;
-        
+        fogDensity = 0;
+        normSpeed = GetComponent<NavMeshAgent>().speed;
+        normAccel = GetComponent<NavMeshAgent>().acceleration;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        float playerDistance = Vector2.Distance(new Vector2(player.transform.position.x, player.transform.position.z), new Vector2(transform.position.x, transform.position.z));
         transform.GetComponent<NavMeshAgent>().destination = player.transform.position;
         if (anim.GetCurrentAnimatorStateInfo(0).IsName("Take 001 2")) {
             clawsActive = true;
         } else {
             clawsActive = false;
         }
-        if (/* not moving &&*/ !anim.GetCurrentAnimatorStateInfo(0).IsName("Take 001") && !anim.GetCurrentAnimatorStateInfo(0).IsName("Take 001 2")) {
+        if (GetComponent<NavMeshAgent>().speed == 0 && !anim.GetCurrentAnimatorStateInfo(0).IsName("Take 001") && !anim.GetCurrentAnimatorStateInfo(0).IsName("Take 001 2")) {
             anim.Play("Take 001", 0, 0f);
         }
+        if(anim.GetCurrentAnimatorStateInfo(0).IsName("Take 001")) {
+            transform.LookAt(new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z));
+        }
+
+        if(playerDistance <= minDistance) {
+            GetComponent<NavMeshAgent>().speed = 0;
+            GetComponent<NavMeshAgent>().acceleration = 1000;
+        }else {
+            GetComponent<NavMeshAgent>().speed = normSpeed;
+            GetComponent<NavMeshAgent>().acceleration = normAccel;
+            //move();
+        }
+
+
+        if(fogDensity < maxFogDensity) {
+            fogDensity += maxFogDensity * Time.deltaTime * fogGrowthRate/100;
+        }else if(fogDensity > maxFogDensity) {
+            fogDensity = maxFogDensity;
+        }
+        RenderSettings.fogDensity = fogDensity;
     }
 
     void move() {
