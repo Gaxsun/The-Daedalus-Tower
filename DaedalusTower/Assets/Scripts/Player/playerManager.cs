754242b12ee003ea240a21c1bdffcaf728182f38
Gaxsun <Razgriz248@gmail.com>
2018-05-29 07:01:24 +1000

    Health Regen

--- DaedalusTower/Assets/Scripts/Player/playerManager.cs
+++ DaedalusTower/Assets/Scripts/Player/playerManager.cs
@@ -10,7 +10,10 @@ public class playerManager : MonoBehaviour {
     public GameObject weaponPosition;
     public Slider healthBar;
     public Canvas can;
+    public GameObject fill;
+    public int healthRegen = 2; // per sec
     public int health = 200;
+    private float secondCount;
 
 	// Use this for initialization
 	void Start () {
@@ -26,9 +29,18 @@ public class playerManager : MonoBehaviour {
             can.enabled = false;
         }
 
-        //if () {
+        print(health);
+        if (gameObject.GetComponent<PlayerMovement>().playerCam.GetComponent<CameraFollow>().bossFight && fill.GetComponent<Image>().color.g < 0.3) {
+            fill.GetComponent<Image>().color = new Color(fill.GetComponent<Image>().color.r, fill.GetComponent<Image>().color.g + Time.deltaTime*0.1f, fill.GetComponent<Image>().color.b + Time.deltaTime * 0.1f);
+        }
 
-        //}
+        if (health + healthRegen <= healthBar.maxValue && Time.time > secondCount + 1) {
+            health = health + healthRegen;
+            secondCount = Time.time;
+        } else if(Time.time > secondCount + 1) {
+            health = Mathf.RoundToInt(healthBar.maxValue);
+            secondCount = Time.time;
+        }
 
     }
 
