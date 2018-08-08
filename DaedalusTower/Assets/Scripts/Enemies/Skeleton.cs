9dbffaba5fe22f5982c2b9724c73bfab661408e6
Gaxsun <Razgriz248@gmail.com>
2018-08-08 19:34:18 +1000

    Mr Skeletal implemented

---
+++ DaedalusTower/Assets/Scripts/Enemies/Skeleton.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Skeleton : MonoBehaviour {
+
+    private GameObject player;
+
+    public Animator anim;
+
+    public float minDistance;
+
+    public int damage;
+    public float attackDelay;
+    private float attackTimer;
+    private int previousAnimationState = 0;
+    // Use this for initialization
+    void Start() {
+        player = GameObject.FindGameObjectWithTag("Player");
+        attackTimer = 0;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        GetComponent<NavMeshAgent>().destination = player.transform.position;
+
+        if (Vector3.Distance(player.transform.position, this.gameObject.transform.position) <= minDistance && !anim.GetCurrentAnimatorStateInfo(0).IsName("idle") && !anim.GetCurrentAnimatorStateInfo(0).IsName("attack") && !anim.GetCurrentAnimatorStateInfo(0).IsName("attack2")) {
+            anim.SetInteger("currentAnimationState", 0);
+            if (Time.time > attackTimer + attackDelay) {
+                print("working");
+                GetComponent<BoxCollider>().enabled = true;
+                attack();
+            }
+        } else if (Vector3.Distance(player.transform.position, this.gameObject.transform.position) > minDistance && !anim.GetCurrentAnimatorStateInfo(0).IsName("walk") && !anim.GetCurrentAnimatorStateInfo(0).IsName("attack")) {
+            anim.SetInteger("currentAnimationState", 1);
+        }
+
+        if (previousAnimationState == 0) {
+            if (anim.GetInteger("currentAnimationState") == 3 || anim.GetInteger("currentAnimationState") == 2) {
+                anim.SetInteger("currentAnimationState",0);
+            }
+        }
+
+        if (anim.GetInteger("currentAnimationState") != 0 && anim.GetInteger("currentAnimationState") != 1 && anim.GetInteger("currentAnimationState") != 2 && anim.GetInteger("currentAnimationState") != 3 && anim.GetInteger("currentAnimationState") != 4 && anim.GetInteger("currentAnimationState") != 5) {
+            Destroy(this);
+        }
+
+        previousAnimationState = anim.GetInteger("currentAnimationState");
+    }
+
+    void attack() {
+        
+        if (!anim.GetCurrentAnimatorStateInfo(0).IsName("attack") && !anim.GetCurrentAnimatorStateInfo(0).IsName("attack2")) {
+            if (Mathf.RoundToInt(Random.Range(3,2)) == 3) {
+                anim.SetInteger("currentAnimationState", 3);
+            } else {
+                anim.SetInteger("currentAnimationState", 2);
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if ((anim.GetCurrentAnimatorStateInfo(0).IsName("attack") || anim.GetCurrentAnimatorStateInfo(0).IsName("attack2")) && other.tag == "Player") {
+            player.GetComponent<playerManager>().takeDamage(damage);
+            GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+
+    public void playDeath() {
+        anim.SetInteger("currentAnimationState", 5);
+    }
+
+}
