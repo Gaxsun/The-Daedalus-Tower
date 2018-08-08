a62c5c2d94a7b5778d7f60fca838267dd50b37d7
AnonymousMadman2 <joshuamvac@gmail.com>
2018-08-08 20:16:23 +1000

    Enemy MinDistance and NavMesh adjustments

--- DaedalusTower/Assets/Scripts/Enemies/BasicEnemy/EnemyNavigation.cs
+++ DaedalusTower/Assets/Scripts/Enemies/BasicEnemy/EnemyNavigation.cs
@@ -4,39 +4,40 @@ using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemyNavigation : MonoBehaviour {
-    
+
     private GameObject player;
 
     public Animator anim;
-    
+
     public float minDistance;
 
     public int damage;
     public float attackDelay;
     private float attackTimer;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         player = GameObject.FindGameObjectWithTag("Player");
         attackTimer = 0;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        GetComponent<NavMeshAgent>().destination = player.transform.position;
-
-        if (Vector3.Distance(player.transform.position, this.gameObject.transform.position) <= minDistance && !anim.GetCurrentAnimatorStateInfo(0).IsName("Take 001") && !anim.GetCurrentAnimatorStateInfo(0).IsName("Take 001 0")) {
-            anim.Play("Take 001", 0, 0f);
-            if(Time.time > attackTimer + attackDelay) {
-                GetComponent<BoxCollider>().enabled = true;
-                attack();
-            }
-        }else if (Vector3.Distance(player.transform.position, this.gameObject.transform.position) > minDistance && !anim.GetCurrentAnimatorStateInfo(0).IsName("Take 001 1") && !anim.GetCurrentAnimatorStateInfo(0).IsName("Take 001 0")) {
-            anim.Play("Take 001 1", 0, 0f);
-        }
-        
     }
 
-    void attack() {
+    // Update is called once per frame
+    void Update() {
+        GetComponent<NavMeshAgent>().destination = player.transform.position;
+        if (Vector3.Distance(player.transform.position, transform.position) <= minDistance) {
+            GetComponent<NavMeshAgent>().destination = transform.position;
+            if (!anim.GetCurrentAnimatorStateInfo(0).IsName("Take 001") && !anim.GetCurrentAnimatorStateInfo(0).IsName("Take 001 0")) {
+                anim.Play("Take 001", 0, 0f);
+                if (Time.time > attackTimer + attackDelay) {
+                    GetComponent<BoxCollider>().enabled = true;
+                    attack();
+                }
+            }
+        } else if (!anim.GetCurrentAnimatorStateInfo(0).IsName("Take 001 1") && !anim.GetCurrentAnimatorStateInfo(0).IsName("Take 001 0")) {
+            anim.Play("Take 001 1", 0, 0f);
+        }
+    }
+
+void attack() {
         if (!anim.GetCurrentAnimatorStateInfo(0).IsName("Take 001 0")) {
             anim.Play("Take 001 0", 0, 0f);
         }
