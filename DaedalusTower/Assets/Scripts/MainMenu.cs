c11d14010017eb66814ce28ed5fc6c6a80e3a9d8
Jake Gilbert <jakegilbert13@hotmail.com>
2018-08-20 16:44:44 +1000

    Loading Screen v1.0

--- DaedalusTower/Assets/Scripts/MainMenu.cs
+++ DaedalusTower/Assets/Scripts/MainMenu.cs
@@ -8,21 +8,66 @@ public class MainMenu : MonoBehaviour {
     public Button playButton;
     public Slider optionSlider;
     public Button backButton;
+    public bool continueButton = false;
 
-    CursorLockMode wantedMode;
+    //CursorLockMode wantedMode;
 
-    public void Awake() {
-        wantedMode = CursorLockMode.Locked;
-    }
+    //public void Awake() {
+    //wantedMode = CursorLockMode.Locked;
+    // }
 
-    public void Update() {
-        Cursor.lockState = wantedMode;
-    }
+    //public void Update() {
+    //Cursor.lockState = wantedMode;
+    // }
 
-    public void PlayGame ()
+
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text progressText;
+    public Button pressToContinue;
+
+    public void PlayGame (int sceneIndex)
 	{
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-	}
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+    IEnumerator LoadAsynchronously (int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        
+        loadingScreen.SetActive(true);
+        
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            slider.value = progress;
+
+            progressText.text = progress * 100f + "%";
+            if (progress >= 1.0f)
+            {
+                progressText.gameObject.SetActive(false);
+                pressToContinue.gameObject.SetActive(true);
+            }
+            else
+            {
+                pressToContinue.gameObject.SetActive(false);
+            }
+
+            yield return null;
+
+            if (continueButton)
+            {
+                operation.allowSceneActivation = true;
+            }
+            else
+            {
+                operation.allowSceneActivation = false;
+            }
+        }
+        
+    }
 
     public void QuitGame()
     {
@@ -40,5 +85,10 @@ public class MainMenu : MonoBehaviour {
     public void OpenControls()
     {
         backButton.Select();
+    }
+
+    public void ContinueScene()
+    {
+        continueButton = true;
     }
 }
